ba295a8ff02d80cd4cf34ebd789515676d1df2e4
Babatunde Alli <tunsmart@gmail.com>
2025-03-23 21:44:10 -0400

    TRy timestamp fix

--- ext/datafusion_ruby/src/record_batch.rs
+++ ext/datafusion_ruby/src/record_batch.rs
@@ -1,6 +1,6 @@
 use datafusion::arrow::{
-    array::{Float64Array, Int64Array, StringArray, StringViewArray},
-    datatypes::DataType,
+    array::{Float64Array, Int64Array, StringArray, StringViewArray, TimestampMillisecondArray},
+    datatypes::{DataType, TimeUnit},
     record_batch::RecordBatch,
 };
 use magnus::{Error, Value};
@@ -22,41 +22,57 @@ impl RbRecordBatch {
         let mut columns_by_name: HashMap<String, Vec<Value>> = HashMap::new();
         for (i, field) in self.rb.schema().fields().iter().enumerate() {
             let column = self.rb.column(i);
-            columns_by_name.insert(
-                field.name().clone(),
-                match column.data_type() {
-                    DataType::Int64 => {
-                        let array = column.as_any().downcast_ref::<Int64Array>().unwrap();
-                        array.values().iter().map(|v| (*v as i64).into()).collect()
-                    }
-                    DataType::Float64 => {
-                        let array = column.as_any().downcast_ref::<Float64Array>().unwrap();
-                        array.values().iter().map(|v| (*v as f64).into()).collect()
-                    }
-                    DataType::Utf8 | DataType::LargeUtf8 | DataType::Utf8View => {
-                        if let Some(array) = column.as_any().downcast_ref::<StringArray>() {
-                            array.iter().map(|opt_v| opt_v.unwrap_or("").to_string().into()).collect()
-                        } else if let Some(array) = column.as_any().downcast_ref::<StringViewArray>() {
-                            array.iter().map(|opt_v| opt_v.unwrap_or("").to_string().into()).collect()
-                        } else {
-                            return Err(DataFusionError::CommonError(format!(
-                                "unhandled string array type: {} (array: {:?})",
-                                column.data_type(),
-                                column.as_ref()
-                            ))
-                            .into());
-                        }
-                    }
-                    unknown => {
+            println!("Processing column '{}' with data type: {:?}", field.name(), column.data_type());
+            println!("Array type: {:?}", column.as_ref());
+            
+            let result = match column.data_type() {
+                DataType::Int64 => {
+                    let array = column.as_any().downcast_ref::<Int64Array>().unwrap();
+                    array.values().iter().map(|v| (*v as i64).into()).collect()
+                }
+                DataType::Float64 => {
+                    let array = column.as_any().downcast_ref::<Float64Array>().unwrap();
+                    array.values().iter().map(|v| (*v as f64).into()).collect()
+                }
+                DataType::Utf8 | DataType::LargeUtf8 | DataType::Utf8View => {
+                    if let Some(array) = column.as_any().downcast_ref::<StringArray>() {
+                        array.iter().map(|opt_v| opt_v.unwrap_or("").to_string().into()).collect()
+                    } else if let Some(array) = column.as_any().downcast_ref::<StringViewArray>() {
+                        array.iter().map(|opt_v| opt_v.unwrap_or("").to_string().into()).collect()
+                    } else {
                         return Err(DataFusionError::CommonError(format!(
-                            "unhandled data type: {} (array: {:?})",
-                            unknown,
+                            "unhandled string array type: {} (array: {:?})",
+                            column.data_type(),
                             column.as_ref()
                         ))
-                        .into())
+                        .into());
                     }
-                },
-            );
+                }
+                DataType::Timestamp(TimeUnit::Millisecond, tz) => {
+                    println!("Attempting to handle timestamp with timezone: {:?}", tz);
+                    if let Some(array) = column.as_any().downcast_ref::<TimestampMillisecondArray>() {
+                        println!("Successfully downcasted to TimestampMillisecondArray");
+                        array.values().iter().map(|ts_millis| (*ts_millis).into()).collect()
+                    } else {
+                        println!("Failed to downcast to TimestampMillisecondArray");
+                        return Err(DataFusionError::CommonError(format!(
+                            "failed to downcast timestamp array: {} (array: {:?})",
+                            column.data_type(),
+                            column.as_ref()
+                        ))
+                        .into());
+                    }
+                }
+                unknown => {
+                    return Err(DataFusionError::CommonError(format!(
+                        "unhandled data type: {} (array: {:?})",
+                        unknown,
+                        column.as_ref()
+                    ))
+                    .into())
+                }
+            };
+            columns_by_name.insert(field.name().clone(), result);
         }
         Ok(columns_by_name)
     }
